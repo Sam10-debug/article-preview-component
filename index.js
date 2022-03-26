2cf070b29ddf37ac8080022d25925939f609dfb4
Emorinken <emorinkensam@gmail.com>
2022-03-26 23:50:54 +0100

    Enter

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-const share= document.querySelector('.share')
-const style= document.createElement('style')
-const sharefunc=()=>{
-    style.innerHTML=`
-    .details{
-        display:none;
+const shareBtn= document.querySelector('#shareBtn')
+const tooltip= document.querySelector('.tooltip')
+shareBtn.addEventListener("click",()=>{
+    tooltip.classList.toggle("active")
+    shareBtn.classList.toggle("active")
+})
+shareBtn.addEventListener("blur",(e)=>{
+    console.log(e.target, e.relatedTarget)
+    if (e.relatedTarget !== tooltip){
+        tooltip.classList.remove("active")
+        shareBtn.classList.remove("active")
     }
-    .images{
-        display: grid;
-    grid-template-columns: 1.5fr 1fr 1fr 2fr 1fr;
-    background-color:  hsl(214, 17%, 51%, 0.914);
-    padding: 2rem;
-    position: absolute;
-    top: 88%;
-    width: 375px;
-    left: 0%;
-    }
-    .textwrap > p{
-        margin-bottom:50px
-    }
-    .shares{
-        margin-left:1rem;
-        margin-right:1rem;
-        width: 35px;
-    height: 35px;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    background-color: hsl(210, 46%, 95%,0.05);
-    transform: translate(0,-55%);
-    }
-    .fb{
-        transform: translate(0,-39%);
-    }
-    .twitter{
-        transform: translate(0,-39%);
-    }
-    .lol{
-        transform: translate(0,-39%);
-        color: hsl(214, 19%, 71%, 1);
-    }
-    .pint{
-        transform: translate(0,-39%);
-    }
-    `;
-    document.head.appendChild(style)
-}
-share.addEventListener("click",sharefunc)
+})
+tooltip.addEventListener("blur", (e)=>{
+    tooltip.classList.remove("active")
+    shareBtn.classList.remove("active")
+})
